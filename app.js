92e5abc4a34e09374d2782880c79acd839d0584b
Marcos Espinoza <marcosprogram96@gmail.com>
2024-03-18 17:39:07 -0600

    encripta todas la vocales pero no las letras

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function encriptarclick(){
     var datosusuario = document.getElementById('texto1').value;
     console.log(`Datos ingresados a encriptar: ${datosusuario}`);
 
-
+    //! variables que cambiaran el texto original
     var firstletter_A = "ay";
     var firstletter_E = "enter";
     var firstletter_I = "imes";
@@ -16,18 +16,28 @@ function encriptarclick(){
             // Reemplazar todas las "a,e,i,o,u" por "ai ,enter,imes,ober,ufat" en el caracter actual
             //*  Utilice exprecion regular. 
             //* La g (global) en la expresión regular asegura
-            //* que se reemplacen todas las "a,e,i,o,u" que se vallan encontrando
-            const palabraencriptada = datosusuario[i]
+            //* que se reemplacen todas las "a,e,i,o,u" que se vallan encontrando en la cadena original
+            var palabraencriptada = datosusuario[i]
             .replace(/a/g, firstletter_A)
             .replace(/e/g, firstletter_E)
             .replace(/i/g, firstletter_I)
             .replace(/o/g, firstletter_O)
             .replace(/u/g, firstletter_U);
             console.log('letra actual:', palabraencriptada);
-       
     }
-}
+   
+    //! Pasar la palabra encriptada al textArea 
+    var mostrarEnTexto2 = palabraencriptada 
+       var texto2 = document.getElementById("texto2");
+             texto2.innerHTML = mostrarEnTexto2;
+  console.log("RESULTADO ENCRIPTADO: ",mostrarEnTexto2 );
 
+    //!   cambiar el h2 a texto encriptado
+    var cambiarEstado = "Texto Encriptado";
+    var nuevoEstado = document.getElementById('cambioEstado');
+    nuevoEstado.innerHTML = cambiarEstado;
+
+}
 
 function actualizarPagina() {
     window.location.reload(); //* Recargar la página
